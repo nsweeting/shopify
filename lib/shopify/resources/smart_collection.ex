18092c8dced94e809a7af21b1d26ec0e8b84e652
humancopy <info@humancopy.net>
2018-04-26 12:09:41 +0200

    Add smart collections

---
+++ lib/shopify/resources/smart_collection.ex
@@ -0,0 +1,49 @@
+defmodule Shopify.SmartCollection do
+  @derive [Poison.Encoder]
+  @singular "smart_collection"
+  @plural "smart_collections"
+
+  use Shopify.Resource,
+    import: [
+      :find,
+      :all,
+      :create,
+      :count,
+      :update,
+      :delete
+    ]
+
+  alias Shopify.{
+    CustomCollection
+  }
+
+  defstruct [
+    :body_html,
+    :handle,
+    :id,
+    :image,
+    :published,
+    :published_at,
+    :published_scope,
+    :rules,
+    :disjunctive,
+    :sort_order,
+    :template_suffix,
+    :title,
+    :updated_at
+  ]
+
+  @doc false
+  def empty_resource do
+    %CustomCollection{}
+  end
+
+  @doc false
+  def find_url(id), do: @plural <> "/#{id}.json"
+
+  @doc false
+  def all_url, do: @plural <> ".json"
+
+  @doc false
+  def count_url, do: @plural <> "/count.json"
+end
