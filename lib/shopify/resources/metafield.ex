4c5a387f8f2f9ccdde7941489633a9abca5878bc
humancopy <info@humancopy.net>
2018-04-26 10:15:51 +0200

    Complete all functions and tests for nested metafields

--- lib/shopify/resources/metafield.ex
+++ lib/shopify/resources/metafield.ex
@@ -6,6 +6,18 @@ defmodule Shopify.Metafield do
   @singular "metafield"
   @plural "metafields"
 
+  @plural_to_singular %{
+    "blogs"        => "blog",
+    "collections"  => "collection",
+    "customers"    => "customer",
+    "draft_orders" => "draft_order",
+    "orders"       => "order",
+    "pages"        => "page",
+    "products"     => "product",
+    "variants"     => "variant",
+    "articles"     => "article"
+  }
+
   use Shopify.Resource,
     import: [
       :find,
@@ -45,19 +57,89 @@ defmodule Shopify.Metafield do
 
   ~w(blogs collections customers draft_orders orders pages products)
   |> Enum.each(fn (name) ->
-    def unquote(:"#{name}")(session, parent_id, params \\ %{}) do
+    singular_name = @plural_to_singular[name]
+    def unquote(:"all_#{singular_name}_metafields")(session, parent_id, params \\ %{}) do
       session
-      |> Request.new(metafield_url(unquote(name), parent_id), params, plural_resource())
+      |> Request.new(metafields_url_for_object(unquote(name), parent_id), params, plural_resource())
       |> Client.get()
+    end
+
+    def unquote(:"find_#{singular_name}_metafield")(session, parent_id, metafield_id) do
+      session
+      |> Request.new(metafield_url_for_object(unquote(name), parent_id, metafield_id), %{}, singular_resource())
+      |> Client.get()
+    end
+
+    def unquote(:"count_#{singular_name}_metafields")(session, parent_id) do
+      session
+      |> Request.new(metafield_url_for_object(unquote(name), parent_id, 'count'), %{}, nil)
+      |> Client.get()
+    end
+
+    def unquote(:"create_#{singular_name}_metafield")(session, parent_id, new_resource) do
+      body = new_resource |> to_json
+
+      session
+      |> Request.new(metafields_url_for_object(unquote(name), parent_id), %{}, singular_resource(), body)
+      |> Client.post()
+    end
+
+    def unquote(:"update_#{singular_name}_metafield")(session, parent_id, metafield_id, updated_resource) do
+      body = updated_resource |> to_json
+
+      session
+      |> Request.new(metafield_url_for_object(unquote(name), parent_id, metafield_id), %{}, singular_resource(), body)
+      |> Client.put()
+    end
+
+    def unquote(:"delete_#{singular_name}_metafield")(session, parent_id, metafield_id) do
+      session
+      |> Request.new(metafield_url_for_object(unquote(name), parent_id, metafield_id), %{}, nil)
+      |> Client.delete()
     end
   end)
 
   [["blogs", "articles"], ["products", "variants"]]
   |> Enum.each(fn ([parent_name, child_name]) ->
-    def unquote(:"#{child_name}")(session, parent_id, child_id, params \\ %{}) do
+    singular_name = "#{@plural_to_singular[parent_name]}_#{@plural_to_singular[child_name]}"
+    def unquote(:"all_#{singular_name}_metafields")(session, parent_id, child_id, params \\ %{}) do
       session
-      |> Request.new(metafield_url(unquote(parent_name), parent_id,unquote(child_name), child_id), params, plural_resource())
+      |> Request.new(metafields_url_for_object(unquote(parent_name), parent_id, unquote(child_name), child_id), params, plural_resource())
       |> Client.get()
+    end
+
+    def unquote(:"find_#{singular_name}_metafield")(session, parent_id, child_id, metafield_id) do
+      session
+      |> Request.new(metafield_url_for_object(unquote(parent_name), parent_id, unquote(child_name), child_id, metafield_id), %{}, singular_resource())
+      |> Client.get()
+    end
+
+    def unquote(:"count_#{singular_name}_metafields")(session, parent_id, child_id) do
+      session
+      |> Request.new(metafield_url_for_object(unquote(parent_name), parent_id, unquote(child_name), child_id, 'count'), %{}, nil)
+      |> Client.get()
+    end
+
+    def unquote(:"create_#{singular_name}_metafield")(session, parent_id, child_id, new_resource) do
+      body = new_resource |> to_json
+
+      session
+      |> Request.new(metafields_url_for_object(unquote(parent_name), parent_id, unquote(child_name), child_id), %{}, singular_resource(), body)
+      |> Client.post()
+    end
+
+    def unquote(:"update_#{singular_name}_metafield")(session, parent_id, child_id, metafield_id, updated_resource) do
+      body = updated_resource |> to_json
+
+      session
+      |> Request.new(metafield_url_for_object(unquote(parent_name), parent_id, unquote(child_name), child_id, metafield_id), %{}, singular_resource(), body)
+      |> Client.put()
+    end
+
+    def unquote(:"delete_#{singular_name}_metafield")(session, parent_id, child_id, metafield_id) do
+      session
+      |> Request.new(metafield_url_for_object(unquote(parent_name), parent_id, unquote(child_name), child_id, metafield_id), %{}, nil)
+      |> Client.delete()
     end
   end)
 
@@ -71,12 +153,22 @@ defmodule Shopify.Metafield do
   def count_url, do: @plural <> "/count.json"
 
   @doc false
-  def metafield_url(parent_name, parent_id) do
+  defp metafields_url_for_object(parent_name, parent_id) do
     parent_name <> "/#{parent_id}/" <> @plural <> ".json"
   end
 
   @doc false
-  def metafield_url(parent_name, parent_id, child_name, child_id) do
+  defp metafields_url_for_object(parent_name, parent_id, child_name, child_id) do
     parent_name <> "/#{parent_id}/" <> child_name <> "/#{child_id}/" <> @plural <> ".json"
+  end
+
+  @doc false
+  defp metafield_url_for_object(parent_name, parent_id, metafield_id) do
+    parent_name <> "/#{parent_id}/" <> @plural <> "/#{metafield_id}.json"
+  end
+
+  @doc false
+  defp metafield_url_for_object(parent_name, parent_id, child_name, child_id, metafield_id) do
+    parent_name <> "/#{parent_id}/" <> child_name <> "/#{child_id}/" <> @plural <> "/#{metafield_id}.json"
   end
 end
---
+++ test/metafield_test.exs
@@ -0,0 +1,186 @@
+defmodule Shopify.MetafieldTest do
+  use ExUnit.Case, async: true
+
+  alias Shopify.Metafield
+
+  test "client can request a single metafield" do
+    assert {:ok, response} = Shopify.session() |> Metafield.find(1)
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+
+    fixture =
+      Fixture.load("../test/fixtures/metafields/1.json", "metafield", Metafield.empty_resource())
+
+    assert fixture == response.data
+  end
+
+  test "client can request all metafields" do
+    assert {:ok, response} = Shopify.session() |> Metafield.all()
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+
+    fixture =
+      Fixture.load("../test/fixtures/metafields.json", "metafields", [Metafield.empty_resource()])
+
+    assert fixture == response.data
+  end
+
+  test "client can request a metafield count" do
+    assert {:ok, response} = Shopify.session() |> Metafield.count()
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+    fixture = Fixture.load("../test/fixtures/metafields/count.json", "count", nil)
+    assert fixture == response.data
+  end
+
+  test "client can request to create a metafield" do
+    fixture =
+      Fixture.load("../test/fixtures/metafields/1.json", "metafield", Metafield.empty_resource())
+
+    assert {:ok, response} = Shopify.session() |> Metafield.create(fixture)
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+
+    fixture =
+      Fixture.load("../test/fixtures/metafields/1.json", "metafield", Metafield.empty_resource())
+
+    assert fixture == response.data
+  end
+
+  test "client can request to update a metafield" do
+    assert {:ok, response} = Shopify.session() |> Metafield.find(1)
+    assert "app_key" == response.data.value
+    update = %{response.data | value: "Update"}
+    assert {:ok, response} = Shopify.session() |> Metafield.update(1, update)
+    assert "Update" == response.data.value
+  end
+
+  test "client can request to delete a metafield" do
+    assert {:ok, response} = Shopify.session() |> Metafield.delete(1)
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+    assert nil == response.data
+  end
+
+  # These tests are specific to parent resources
+
+  test "client can request a single metafield for product" do
+    assert {:ok, response} = Shopify.session() |> Metafield.find_product_metafield(1, 1)
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+
+    fixture =
+      Fixture.load("../test/fixtures/products/1/metafields/1.json", "metafield", Metafield.empty_resource())
+
+    assert fixture == response.data
+  end
+
+  test "client can request all metafields for product" do
+    assert {:ok, response} = Shopify.session() |> Metafield.all_product_metafields(1)
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+
+    fixture =
+      Fixture.load("../test/fixtures/products/1/metafields.json", "metafields", [Metafield.empty_resource()])
+
+    assert fixture == response.data
+  end
+
+  test "client can request a metafield count for product" do
+    assert {:ok, response} = Shopify.session() |> Metafield.count_product_metafields(1)
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+    fixture = Fixture.load("../test/fixtures/products/1/metafields/count.json", "count", nil)
+    assert fixture == response.data
+  end
+
+  test "client can request to create a metafield for product" do
+    fixture =
+      Fixture.load("../test/fixtures/products/1/metafields/1.json", "metafield", Metafield.empty_resource())
+
+    assert {:ok, response} = Shopify.session() |> Metafield.create_product_metafield(1, fixture)
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+
+    fixture =
+      Fixture.load("../test/fixtures/products/1/metafields/1.json", "metafield", Metafield.empty_resource())
+
+    assert fixture == response.data
+  end
+
+  test "client can request to update a metafield for product" do
+    assert {:ok, response} = Shopify.session() |> Metafield.find_product_metafield(1, 1)
+    assert "produit" == response.data.value
+    update = %{response.data | value: "Update"}
+    assert {:ok, response} = Shopify.session() |> Metafield.update_product_metafield(1, 1, update)
+    assert "Update" == response.data.value
+  end
+
+  test "client can request to delete a metafield for product" do
+    assert {:ok, response} = Shopify.session() |> Metafield.delete_product_metafield(1, 1)
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+    assert nil == response.data
+  end
+
+  # These tests are specific to parent / child resources
+
+  test "client can request a single metafield for product variant" do
+    assert {:ok, response} = Shopify.session() |> Metafield.find_product_variant_metafield(1, 1, 1)
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+
+    fixture =
+      Fixture.load("../test/fixtures/products/1/variants/1/metafields/1.json", "metafield", Metafield.empty_resource())
+
+    assert fixture == response.data
+  end
+
+  test "client can request all metafields for product variant" do
+    assert {:ok, response} = Shopify.session() |> Metafield.all_product_variant_metafields(1, 1)
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+
+    fixture =
+      Fixture.load("../test/fixtures/products/1/variants/1/metafields.json", "metafields", [Metafield.empty_resource()])
+
+    assert fixture == response.data
+  end
+
+  test "client can request a metafield count for product variant" do
+    assert {:ok, response} = Shopify.session() |> Metafield.count_product_variant_metafields(1, 1)
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+    fixture = Fixture.load("../test/fixtures/products/1/variants/1/metafields/count.json", "count", nil)
+    assert fixture == response.data
+  end
+
+  test "client can request to create a metafield for product variant" do
+    fixture =
+      Fixture.load("../test/fixtures/products/1/variants/1/metafields/1.json", "metafield", Metafield.empty_resource())
+
+    assert {:ok, response} = Shopify.session() |> Metafield.create_product_variant_metafield(1, 1, fixture)
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+
+    fixture =
+      Fixture.load("../test/fixtures/products/1/variants/1/metafields/1.json", "metafield", Metafield.empty_resource())
+
+    assert fixture == response.data
+  end
+
+  test "client can request to update a metafield for product variant" do
+    assert {:ok, response} = Shopify.session() |> Metafield.find_product_variant_metafield(1, 1, 1)
+    assert "variante de produit" == response.data.value
+    update = %{response.data | value: "Update"}
+    assert {:ok, response} = Shopify.session() |> Metafield.update_product_variant_metafield(1, 1, 1, update)
+    assert "Update" == response.data.value
+  end
+
+  test "client can request to delete a metafield for product variant" do
+    assert {:ok, response} = Shopify.session() |> Metafield.delete_product_variant_metafield(1, 1, 1)
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+    assert nil == response.data
+  end
+end
