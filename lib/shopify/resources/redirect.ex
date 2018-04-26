9ddf7d58b04348577db9a44967c10bb750309116
humancopy <info@humancopy.net>
2018-04-26 11:08:16 +0200

    Add the redirect resource

---
+++ lib/shopify/resources/redirect.ex
@@ -0,0 +1,40 @@
+defmodule Shopify.Redirect do
+  @derive [Poison.Encoder]
+  @singular "redirect"
+  @plural "redirects"
+
+  use Shopify.Resource,
+    import: [
+      :find,
+      :all,
+      :count,
+      :create,
+      :update,
+      :delete
+    ]
+
+  alias Shopify.{
+    Redirect,
+  }
+
+  defstruct [
+    :id,
+    :path,
+    :target
+  ]
+
+  @doc false
+  def empty_resource do
+    %Redirect{
+    }
+  end
+
+  @doc false
+  def find_url(id), do: @plural <> "/#{id}.json"
+
+  @doc false
+  def all_url, do: @plural <> ".json"
+
+  @doc false
+  def count_url, do: @plural <> "/count.json"
+end
---
+++ test/redirect_test.exs
@@ -0,0 +1,53 @@
+defmodule Shopify.RedirectTest do
+  use ExUnit.Case, async: true
+
+  alias Shopify.Redirect
+
+  test "client can request a single redirect" do
+    assert {:ok, response} = Shopify.session() |> Redirect.find(1)
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+    fixture = Fixture.load("../test/fixtures/redirects/1.json", "redirect", Redirect.empty_resource())
+    assert fixture == response.data
+  end
+
+  test "client can request all redirects" do
+    assert {:ok, response} = Shopify.session() |> Redirect.all()
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+    fixture = Fixture.load("../test/fixtures/redirects.json", "redirects", [Redirect.empty_resource()])
+    assert fixture == response.data
+  end
+
+  test "client can request a nredirect count" do
+    assert {:ok, response} = Shopify.session() |> Redirect.count()
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+    fixture = Fixture.load("../test/fixtures/redirects/count.json", "count", nil)
+    assert fixture == response.data
+  end
+
+  test "client can request to create an redirect" do
+    fixture = Fixture.load("../test/fixtures/redirects/1.json", "redirect", Redirect.empty_resource())
+    assert {:ok, response} = Shopify.session() |> Redirect.create(fixture)
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+    fixture = Fixture.load("../test/fixtures/redirects/1.json", "redirect", Redirect.empty_resource())
+    assert fixture == response.data
+  end
+
+  test "client can request to update an redirect" do
+    assert {:ok, response} = Shopify.session() |> Redirect.find(1)
+    assert "/leopard" == response.data.path
+    update = %{response.data | path: "Update"}
+    assert {:ok, response} = Shopify.session() |> Redirect.update(1, update)
+    assert "Update" == response.data.path
+  end
+
+  test "client can request to delete an redirect" do
+    assert {:ok, response} = Shopify.session() |> Redirect.delete(1)
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+    assert nil == response.data
+  end
+end
