9b3aa632cf95fb39083a2f709f82bc91154dcf1d
Fabian Zitter <fabian.zitter@gmail.com>
2018-05-03 13:57:25 +0900

    Add fulfillment events

--- lib/shopify/resources/order/fulfillment/event.ex
+++ lib/shopify/resources/order/fulfillment/event.ex
@@ -1,12 +1,43 @@
 defmodule Shopify.Order.Fulfillment.Event do
-  @singular "event"
-  @plural "events"
+  @singular "fulfillment_event"
+  @plural "fulfillment_events"
 
-  use Shopify.NestedResource, import: [:all]
+  use Shopify.Resource
 
   @doc false
   def empty_resource, do: Shopify.Event.empty_resource()
 
+  def all(%Shopify.Session{} = session, order_id, fulfillment_id, params \\ %{}) do
+    session
+    |> Request.new(all_url(order_id, fulfillment_id), params, plural_resource())
+    |> Client.get()
+  end
+
+  def find(%Shopify.Session{} = session, order_id, fulfillment_id, event_id, params \\ %{}) do
+    session
+    |> Request.new(find_url(order_id, fulfillment_id, event_id), params, singular_resource())
+    |> Client.get()
+  end
+
+  def create(%Session{} = session, order_id, fulfillment_id, new_resource) do
+    body = new_resource |> to_json
+
+    session
+    |> Request.new(all_url(order_id, fulfillment_id), %{}, singular_resource(), body)
+    |> Client.post()
+  end
+
+  def delete(%Session{} = session, order_id, fulfillment_id, event_id) do
+    session
+    |> Request.new(find_url(order_id, fulfillment_id, event_id), %{}, nil)
+    |> Client.delete()
+  end
+
   @doc false
-  def all_url(order_id), do: "orders/#{order_id}/" <> "events.json"
+  def all_url(order_id, fulfillment_id), do:  base_url(order_id, fulfillment_id) <> ".json"
+
+  @doc false
+  def find_url(order_id, fulfillment_id, event_id), do: base_url(order_id, fulfillment_id) <> "/#{event_id}.json"
+
+  defp base_url(order_id, fulfillment_id), do: "orders/#{order_id}/fulfillments/#{fulfillment_id}/events"
 end
---
+++ test/order/fulfillment/event_test.exs
@@ -0,0 +1,37 @@
+defmodule Shopify.Order.Fulfillment.EventTest do
+  use ExUnit.Case, async: true
+
+  alias Shopify.Order.Fulfillment.Event
+
+  test "client can request a single event" do
+    assert {:ok, response} = Shopify.session() |> Event.find(1, 1, 1)
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+    fixture = Fixture.load("../test/fixtures/orders/1/fulfillments/1/events/1.json", "fulfillment_event", Event.empty_resource())
+    assert fixture == response.data
+  end
+
+  test "client can request all events" do
+    assert {:ok, response} = Shopify.session() |> Event.all(1, 1)
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+    fixture = Fixture.load("../test/fixtures/orders/1/fulfillments/1/events.json", "fulfillment_events", [Event.empty_resource()])
+    assert fixture == response.data
+  end
+
+  test "client can request to create a event" do
+    fixture = Fixture.load("../test/fixtures/orders/1/fulfillments/1/events/1.json", "fulfillment_event", Event.empty_resource())
+    assert {:ok, response} = Shopify.session() |> Event.create(1, 1, fixture)
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+    fixture = Fixture.load("../test/fixtures/orders/1/fulfillments/1/events/1.json", "fulfillment_event", Event.empty_resource())
+    assert fixture == response.data
+  end
+
+  test "client can request to delete an event" do
+    assert {:ok, response} = Shopify.session() |> Event.delete(1, 1, 1)
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+    assert nil == response.data
+  end
+end
