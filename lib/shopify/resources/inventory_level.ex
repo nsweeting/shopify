028036d194aef2f68ca49bdc55e44f017c1d38af
Fabian Zitter <fabian.zitter@gmail.com>
2018-05-03 16:12:23 +0900

    Add inventory level

---
+++ lib/shopify/resources/inventory_level.ex
@@ -0,0 +1,110 @@
+defmodule Shopify.InventoryLevel do
+  @derive [Poison.Encoder]
+  @singular "inventory_level"
+  @plural "inventory_levels"
+
+  @missing_all_params_msg "InventoryLevel.all/2 needs an inventory_item_ids or locations_ids parameter."
+  @missing_adjustment_parameters "InventoryLevel.adjust/2 needs an inventory_item_id, location_id, and available_adjustment parameter."
+  @missing_connect_parameters "InventoryLevel.connect/2 needs an inventory_item_id, and a location_id parameter."
+
+  use Shopify.Resource,
+    import: [
+      :delete
+    ]
+
+  defstruct [
+    :id,
+    :sku,
+    :tracked,
+    :created_at,
+    :updated_at
+  ]
+
+  @doc false
+  def empty_resource do
+    %__MODULE__{}
+  end
+
+  @doc """
+  Requests all resources. Needs an array of inventory item ids or location ids as parameters!
+
+  Returns `{:ok, %Shopify.Response{}}` or `{:error, %Shopify.Response{}}`
+
+  ## Parameters
+    - session: A `%Shopify.Session{}` struct.
+    - params: Can be inventory_item_ids or location_ids or both.
+
+  ## Examples
+      iex> Shopify.session |> Shopify.InventoryLevel.all(%{inventory_item_ids: [123]})
+      {:ok, %Shopify.Response{}}
+  """
+  @spec all(%Shopify.Session{}, map) :: %Shopify.Response{}
+  def all(session, params)
+  def all(%Shopify.Session{} = session, %{inventory_item_ids: _} = params),
+    do: do_all(session, params)
+  def all(%Shopify.Session{} = session, %{location_ids: _} = params),
+    do: do_all(session, params)
+  def all(%Shopify.Session{}, _params),
+    do: unprocessable_entity(@missing_all_params_msg)
+
+  defp do_all(session, params) do
+    session
+    |> Request.new(all_url(), params, plural_resource())
+    |> Client.get()
+  end
+
+  @doc """
+  Adjusts the inventory for a given inventory item at a given location.
+
+  Returns `{:ok, %Shopify.Response{}}` or `{:error, %Shopify.Response{}}`
+
+  ## Parameters
+    - session: A `%Shopify.Session{}` struct.
+    - params: Any additional query params.
+
+  ## Examples
+      iex> Shopify.session |> Shopify.InventoryLevel.adjust(%{inventory_item_id: 123, location_id: 123, available_adjustment: 123})
+      {:ok, %Shopify.Response{}}
+  """
+  @spec adjust(%Shopify.Session{}, map) :: %Shopify.Response{}
+  def adjust(%Shopify.Session{} = session, %{inventory_item_id: _, location_id: _, available_adjustment: _} = inventory_level) do
+    session
+    |> Request.new(@plural <> "/adjust.json", inventory_level, singular_resource())
+    |> Client.post()
+  end
+
+  def adjust(%Shopify.Session{}, _),
+    do: unprocessable_entity(@missing_adjustment_parameters)
+
+  @doc """
+  Connects a given inventory with a location.
+
+  Returns `{:ok, %Shopify.Response{}}` or `{:error, %Shopify.Response{}}`
+
+  ## Parameters
+    - session: A `%Shopify.Session{}` struct.
+    - params: Any additional query params.
+
+  ## Examples
+      iex> Shopify.session |> Shopify.InventoryLevel.connect(%{inventory_item_id: 123, location_id: 123})
+      {:ok, %Shopify.Response{}}
+  """
+  @spec connect(%Shopify.Session{}, map) :: %Shopify.Response{}
+  def connect(%Shopify.Session{} = session, %{inventory_item_id: _, location_id: _} = connection) do
+    session
+    |> Request.new(@plural <> "/connect.json", connection, singular_resource())
+    |> Client.post()
+  end
+
+  def connect(%Shopify.Session{}, _),
+    do: unprocessable_entity(@missing_connect_parameters)
+
+  @doc false
+  def find_url(id), do: @plural <> "/#{id}.json"
+
+  @doc false
+  def all_url, do: @plural <> ".json"
+
+  defp unprocessable_entity(msg),
+    do: Shopify.Response.new(422, msg, empty_resource())
+end
--- test/inventory_level_test.exs
+++ test/inventory_level_test.exs
@@ -11,8 +11,8 @@ defmodule Shopify.InventoryLevelTest do
     assert fixture == response.data
   end
 
-  test "client responds with error 422 if no location_ids or inventory_item_ids parameter" do
-    {success, %{code: code}} = Shopify.session() |> InventoryLevel.all()
+  test "client responds with error 422 if no location_ids or inventory_item_ids parameter for all/2" do
+    {success, %{code: code}} = Shopify.session() |> InventoryLevel.all(%{something_else: "asdf"})
 
     assert success == :error
     assert code == 422
@@ -23,5 +23,41 @@ defmodule Shopify.InventoryLevelTest do
     assert %Shopify.Response{} = response
     assert 200 == response.code
     assert nil == response.data
+  end
+
+  test "client can request an adjustment" do
+    params = %{inventory_item_id: 123, location_id: 123, available_adjustment: 123}
+
+    assert {:ok, resp} = Shopify.session() |> InventoryLevel.adjust(params)
+    fixture = Fixture.load("../test/fixtures/inventory_levels/adjust.json", "inventory_level", InventoryLevel.empty_resource())
+    assert resp.code == 200
+    assert resp.data == fixture
+  end
+
+  test "client responds with error 422 if parameters missing for adjust/2" do
+    params = %{inventory_item_id: 123, location_id: 123}
+
+    {success, resp} = Shopify.session() |> InventoryLevel.adjust(params)
+
+    assert success == :error
+    assert resp.code == 422
+  end
+
+  test "client can request to connect inventory to a location" do
+    params = %{inventory_item_id: 123, location_id: 123}
+
+    assert {:ok, resp} = Shopify.session() |> InventoryLevel.connect(params)
+    fixture = Fixture.load("../test/fixtures/inventory_levels/connect.json", "inventory_level", InventoryLevel.empty_resource())
+    assert resp.code == 200
+    assert resp.data == fixture
+  end
+
+  test "client responds with error 422 if parameters missing for connect/2" do
+    params = %{inventory_item_id: 123}
+
+    {success, resp} = Shopify.session() |> InventoryLevel.connect(params)
+
+    assert success == :error
+    assert resp.code == 422
   end
 end
