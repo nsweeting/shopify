fca95324d3b4ac24c0e6be415cb51cffc0a844db
Fabian Zitter <fabian.zitter@gmail.com>
2018-05-03 14:31:24 +0900

    Add fulfillment services

---
+++ lib/shopify/resources/inventory_item.ex
@@ -0,0 +1,31 @@
+defmodule Shopify.InventoryItem do
+  @derive [Poison.Encoder]
+  @singular "inventory_item"
+  @plural "inventory_items"
+
+  use Shopify.Resource,
+    import: [
+      :find,
+      :all,
+      :update
+    ]
+
+  defstruct [
+    :id,
+    :sku,
+    :tracked,
+    :created_at,
+    :updated_at
+  ]
+
+  @doc false
+  def empty_resource do
+    %__MODULE__{}
+  end
+
+  @doc false
+  def find_url(id), do: @plural <> "/#{id}.json"
+
+  @doc false
+  def all_url, do: @plural <> ".json"
+end
