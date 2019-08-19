271f1f883724b8d7e7baded303bff7def3afac3b
Cole Meurer <cole.meurer@gmail.com>
2019-08-19 11:41:16 +0900

    Create and update functionality for theme assets (#84)

--- lib/shopify/resources/theme/asset.ex
+++ lib/shopify/resources/theme/asset.ex
@@ -3,6 +3,8 @@ defmodule Shopify.Theme.Asset do
   @singular "asset"
   @plural "assets"
 
+  alias Shopify.{Client, Request, Session}
+
   use Shopify.NestedResource,
     import: [
       :all,
@@ -35,4 +37,48 @@ defmodule Shopify.Theme.Asset do
 
   @doc false
   defp url_prefix(theme_id), do: "themes/#{theme_id}"
+
+  @doc """
+  Requests to create a new resource.
+
+  Returns `{:ok, %Shopify.Response{}}` or `{:error, %Shopify.Response{}}`
+
+  ## Parameters
+    - session: A `%Shopify.Session{}` struct.
+    - top_id: The id of the top-level resource.
+    - new_resource: A struct of the resource being created.
+
+  ## Examples
+      iex> Shopify.session |> Shopify.Theme.Asset.create(theme_id)
+      {:ok, %Shopify.Response{}}
+  """
+  def create(%Session{} = session, top_id, new_resource) do
+    body = new_resource |> to_json
+
+    session
+    |> Request.new(all_url(top_id), %{}, empty_resource(), body)
+    |> Client.put()
+  end
+
+  @doc """
+  Requests to update a resource by id.
+
+  Returns `{:ok, %Shopify.Response{}}` or `{:error, %Shopify.Error{}}`
+
+  ## Parameters
+    - session: A `%Shopify.Session{}` struct.
+    - top_id: The id of the top-level resource.
+    - updated_resource: A struct of the resource being updated.
+
+  ## Examples
+      iex> Shopify.session |> Shopify.Theme.Asset.update(theme_id)
+      {:ok, %Shopify.Response{}}
+  """
+  def update(%Session{} = session, top_id, updated_resource) do
+    body = updated_resource |> to_json
+
+    session
+    |> Request.new(all_url(top_id), %{}, empty_resource(), body)
+    |> Client.put()
+  end
 end
