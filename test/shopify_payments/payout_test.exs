7748666145249ca9d390ef73292e740af06fbc81
Lars Kluge <l@larskluge.com>
2019-11-08 12:48:26 +0530

    Shopify Payments: Payouts

---
+++ test/shopify_payments/payout_test.exs
@@ -0,0 +1,21 @@
+defmodule Shopify.ShopifyPayments.PayoutTest do
+  use ExUnit.Case, async: true
+
+  alias Shopify.ShopifyPayments.Payout
+
+  test "client can request a single payout" do
+    assert {:ok, response} = Shopify.session() |> Payout.find(1)
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+    fixture = Fixture.load("../test/fixtures/shopify_payments/payouts/1.json", "payout", Payout.empty_resource())
+    assert fixture == response.data
+  end
+
+  test "client can request all payouts" do
+    assert {:ok, response} = Shopify.session() |> Payout.all()
+    assert %Shopify.Response{} = response
+    assert 200 == response.code
+    fixture = Fixture.load("../test/fixtures/shopify_payments/payouts.json", "payouts", [Payout.empty_resource()])
+    assert fixture == response.data
+  end
+end
